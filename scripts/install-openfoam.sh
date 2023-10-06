d60d80b263ad74572400bd80f7246c5ad5e35416
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2023-10-06 09:15:35 +0100

    Fix call in third-party in OpenFOAM install script
    I think this went under the radar as the third party stuff is built when
    OpenFOAM itself is.

--- scripts/install-openfoam.sh
+++ scripts/install-openfoam.sh
@@ -12,10 +12,13 @@ Note that you will need to install OpenFOAM's requirements separately.
 You can do this by running:
 
     apt install \\
+        bison \\
         flex \\
+        libptscotch-dev \\
         libqt5opengl5-dev \\
         libqt5x11extras5-dev \\
         libxt-dev \\
+        make \\
         mpich \\
         paraview \\
         paraview-dev \\
@@ -79,7 +82,7 @@ wget -O - http://dl.openfoam.org/third-party/10 | tar xvz
 mv "ThirdParty-10-version-10" "${THIRDPARTY_DIR}"
 (
     cd "${THIRDPARTY_DIR}" \
-    && echo ./Allwmake
+    && ./Allwmake
 )
 
 # Refresh OpenFOAM paths
