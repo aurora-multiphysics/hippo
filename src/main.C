2f446c58a95740eb399cffeb47372d861f82b8df
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2025-02-13 11:46:34 +0000

    Fix MOOSE runtime deprecation warnings

--- src/main.C
+++ src/main.C
@@ -9,29 +9,13 @@
 
 #include "hippoTestApp.h"
 
-#include <MooseInit.h>
-#include <Moose.h>
-#include <MooseApp.h>
-#include <AppFactory.h>
+#include <MooseMain.h>
 
 // Create a performance log
 PerfLog Moose::perf_log("hippo");
 
-// Begin the main program.
 int
 main(int argc, char * argv[])
 {
-  // Initialize MPI, solvers and MOOSE
-  MooseInit init(argc, argv);
-
-  // Register this application's MooseApp and any it depends on
-  hippoTestApp::registerApps();
-
-  // Create an instance of the application and store it in a smart pointer for easy cleanup
-  std::shared_ptr<MooseApp> app = AppFactory::createAppShared("hippoTestApp", argc, argv);
-
-  // Execute the application
-  app->run();
-
-  return 0;
+  return Moose::main<hippoTestApp>(argc, argv);
 }
