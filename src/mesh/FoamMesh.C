9501406bc35a5741a64d18d5dc8e4ad5517bc792
Matthew Falcone <mapfalcone@gmail.com>
2025-06-02 15:50:43 +0100

    Handle split edges with polygonal elements

--- src/mesh/FoamMesh.C
+++ src/mesh/FoamMesh.C
@@ -5,6 +5,7 @@
 #include "libmesh/face_quad4.h"
 #include "libmesh/face_c0polygon.h"
 #include "libmesh/face_tri3.h"
+#include "libmesh/point.h"
 
 #include <IOobject.H>
 #include <Pstream/mpi/PstreamGlobals.H>
@@ -89,6 +90,79 @@ FoamMesh::getSubdomainList()
   return _subdomain_list;
 }
 
+inline bool
+checkPointOnLine(const libMesh::Node * pt,
+                 const libMesh::Node * start,
+                 const libMesh::Node * end,
+                 const double tol = 1e-6)
+{
+  // Check if the point is on the line segment defined by start and end
+  auto AP = *pt - *start;
+  auto AB = *end - *start;
+
+  auto cross_product = AB.cross(AP);
+  if (cross_product.norm() > tol)
+    return false; // Not collinear
+
+  auto dot = AB * AP;
+  if (dot < -tol)
+    return false; // Point is before the start of the segment
+  if (dot > AB.norm_sq() + tol)
+    return false;
+
+  return true;
+}
+std::unique_ptr<Elem>
+FoamMesh::createElement(Hippo::Foam2MooseMeshAdapter * mesh_adapter, const Hippo::FoamFace & face)
+{
+  // get all MOOSE nodes associated with points in Foam face
+  std::vector<libMesh::Node *> points;
+  for (auto point = face.begin(); point < face.end(); ++point)
+  {
+    auto moose_pt = mesh_adapter->getMooseId(*point);
+    points.push_back(_mesh->node_ptr(moose_pt));
+  }
+
+  // Remove collinear points on edge (due to more than one neighbouring
+  // elements on this edge neigh)
+
+  // check last point
+  while (checkPointOnLine(points.back(), points.front(), points.end()[-2]))
+    points.pop_back(); // remove last point if it is the same as the first
+
+  // check in reverse the middle points
+  // we start from the second last point and go to the second point
+  for (auto point = points.end() - 2; point != points.begin(); --point)
+  {
+    int idx = point - points.begin();
+    if (checkPointOnLine(*point, *(point - 1), *(point + 1)))
+      points.erase(point); // remove point if it is the same as the previous one
+  }
+  // check first point
+  if (checkPointOnLine(points.front(), points.back(), points[1]))
+    points.erase(points.begin()); // remove first point if it is the same as the last one
+
+  // Create element based on number of points
+  std::unique_ptr<libMesh::Elem> elem;
+  if (points.size() == 3)
+    elem = std::make_unique<libMesh::Tri3>();
+  else if (points.size() == 4)
+    elem = std::make_unique<libMesh::Quad4>();
+  else
+    elem = std::make_unique<libMesh::C0Polygon>(points.size());
+
+  // set points, ranks and subdomain ids
+  int count = 0;
+  for (auto point : points)
+  {
+    elem->set_node(count++) = point;
+  }
+  elem->subdomain_id() = face.subdomain_id();
+  elem->processor_id() = face.rank();
+
+  return elem;
+}
+
 void
 FoamMesh::buildMesh()
 {
@@ -107,19 +181,7 @@ FoamMesh::buildMesh()
   for (int32_t fc = 0; fc < mesh_adapter->nface(); ++fc)
   {
     auto face = mesh_adapter->face(fc);
-    auto elem = _mesh->add_elem(getElement(face));
-    elem->set_id(fc);
-
-    int count = 0;
-    for (auto point = face.begin(); point < face.end(); ++point)
-    {
-      auto moose_pt = mesh_adapter->getMooseId(*point);
-      elem->set_node(count++) = _mesh->node_ptr(moose_pt);
-    }
-    // TODO: need to look again at the subdomain_id so it matches the foam
-    // patch_id?
-    elem->subdomain_id() = face.subdomain_id();
-    elem->processor_id() = face.rank();
+    _mesh->add_elem(createElement(mesh_adapter.get(), face));
   }
 
   // patch id has the openfoam id that corresponds to the patch name
