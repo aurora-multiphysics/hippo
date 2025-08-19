a047da0f560a7b04abd662b1de9d13cdb9da9a97
Matthew Falcone <mapfalcone@gmail.com>
2025-08-19 14:57:25 +0100

    Add laplacian fixed-value fixed-point test

--- test/tests/fixed-point/function_test/test.py
+++ test/tests/fixed-point/function_test/test.py
@@ -2,11 +2,21 @@
 from unittest import TestCase
 from pathlib import Path
 import pyvista as pv
+import fluidfoam as ff
+from glob import glob
+import re
+import os
 import numpy as np
+
+
+def get_foam_times(case_dir: str | bytes):
+    return sorted(np.float64(folder) for folder in os.listdir(case_dir) if re.match(r"[-+]?([0-9]*\.[0-9]+|[0-9]+)", folder))
+
 
 class TestFunctionSolver(TestCase):
     """Test fixed-point iteration where the solver imposes the current time on the internal field.
     """
+
     def test_times(self):
         """
         Test that internal field equals the current time and that old field is the previous time.
@@ -17,22 +27,18 @@ class TestFunctionSolver(TestCase):
 
         # read from openfoam file
         case_dir = 'foam/'
-        file_name = Path(case_dir) / "case.foam"
-        file_name.touch()
-
-        reader: pv.POpenFOAMReader = pv.get_reader(file_name)
-        times = reader.time_values
-        for i in range(len(times)):
+        time_dirs = get_foam_times(case_dir)
+        for i, time in enumerate(time_dirs):
             # set time and read internal field
-            reader.set_active_time_point(i)
-            data: pv.UnstructuredGrid = reader.read()['internalMesh']
+            T = ff.readof.readscalar(
+                case_dir, time_name=f"{time:g}", name='T', verbose=False)
             # check T array equals the time
-            assert np.allclose(data.cell_data['T'], reader.time_values[i], atol=0, rtol=1e-12)
+            assert np.allclose(T, time,
+                                   atol=0, rtol=1e-16), f"Max diff: {abs(T-time).max()}"
 
             # check that next `old time' is equal to the present time
-            if i < len(times) -1:
-                reader.set_active_time_point(i+1)
-                data1: pv.UnstructuredGrid = reader.read()['internalMesh']
-                assert np.allclose(data.cell_data['T'], data1.cell_data['T2'], atol=0, rtol=1e-12)
-
-        file_name.unlink()
+            if i < len(time_dirs) - 1:
+                T2 = ff.readof.readscalar(
+                    case_dir, time_name=f"{time_dirs[i+1]:g}", name='T2', verbose=False)
+                assert np.allclose(T2, time,
+                                   atol=0, rtol=1e-16), f"Max diff: {abs(T2-time).max()}"
