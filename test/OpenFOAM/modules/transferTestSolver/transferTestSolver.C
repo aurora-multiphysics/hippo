a80280c4bf7eaf989d19b6ffb27d10dfec3adba3
Matthew Falcone <mapfalcone@gmail.com>
2025-09-12 10:14:41 +0100

    Implement initial test solver for foam variable shadowing

---
+++ test/OpenFOAM/modules/transferTestSolver/transferTestSolver.C
@@ -0,0 +1,158 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2022-2024 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dimensionSets.H"
+#include "fvMesh.H"
+#include "transferTestSolver.H"
+#include "fvMeshMover.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fvcDdt.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace solvers
+{
+defineTypeNameAndDebug(transferTestSolver, 0);
+addToRunTimeSelectionTable(solver, transferTestSolver, fvMesh);
+}
+}
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+// * * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * //
+
+bool
+Foam::solvers::transferTestSolver::dependenciesModified() const
+{
+  return runTime.controlDict().modified();
+}
+
+bool
+Foam::solvers::transferTestSolver::read()
+{
+  solver::read();
+
+  maxDeltaT_ = runTime.controlDict().found("maxDeltaT")
+                   ? runTime.controlDict().lookup<scalar>("maxDeltaT", runTime.userUnits())
+                   : vGreat;
+
+  return true;
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::solvers::transferTestSolver::transferTestSolver(fvMesh & mesh)
+  : solver(mesh),
+    T_(IOobject("T", mesh.time().name(), mesh, IOobject::NO_READ, IOobject::AUTO_WRITE), mesh),
+    kappa_(IOobject("kappa", mesh.time().name(), mesh, IOobject::NO_READ, IOobject::AUTO_WRITE),
+           mesh,
+           1.),
+    T(T_),
+    kappa(kappa_)
+
+{
+  // Read the controls
+  read();
+}
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::solvers::transferTestSolver::~transferTestSolver() {}
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+Foam::scalar
+Foam::solvers::transferTestSolver::maxDeltaT() const
+{
+  return min(fvModels().maxDeltaT(), maxDeltaT_);
+}
+
+void
+Foam::solvers::transferTestSolver::preSolve()
+{
+  fvModels().preUpdateMesh();
+
+  // Update the mesh for topology change, mesh to mesh mapping
+  mesh_.update();
+}
+
+void
+Foam::solvers::transferTestSolver::moveMesh()
+{
+  if (pimple.firstIter() || pimple.moveMeshOuterCorrectors())
+  {
+    if (!mesh_.mover().solidBody())
+    {
+      FatalErrorInFunction << "Region " << name() << " of type " << type()
+                           << " does not support non-solid body mesh motion" << exit(FatalError);
+    }
+
+    mesh_.move();
+  }
+}
+
+void
+Foam::solvers::transferTestSolver::motionCorrector()
+{
+}
+
+void
+Foam::solvers::transferTestSolver::prePredictor()
+{
+}
+
+void
+Foam::solvers::transferTestSolver::momentumPredictor()
+{
+}
+
+void
+Foam::solvers::transferTestSolver::thermophysicalPredictor()
+{
+  // Set T to the current time
+  dimensioned<Foam::scalar> t(
+      "t", dimTemperature / (dimLength * dimLength * dimLength), mesh_.time().userTimeValue());
+  auto & coords = mesh_.C();
+  T_ == coords.component(0) * coords.component(1) * coords.component(2) * t;
+}
+
+void
+Foam::solvers::transferTestSolver::pressureCorrector()
+{
+}
+
+void
+Foam::solvers::transferTestSolver::postCorrector()
+{
+}
+
+void
+Foam::solvers::transferTestSolver::postSolve()
+{
+}
+
+// ************************************************************************* //
