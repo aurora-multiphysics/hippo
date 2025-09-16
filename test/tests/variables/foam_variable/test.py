a721416c7013c46cefbc818329958c9730719aad
Matthew Falcone <mapfalcone@gmail.com>
2025-09-16 10:12:44 +0100

    Implement more rigorous test for wallHeatFlux

--- test/tests/variables/foam_variable/test.py
+++ test/tests/variables/foam_variable/test.py
@@ -13,7 +13,7 @@ class TestFoamVariableTransfer(unittest.TestCase):
                                                  time,
                                                  variable="T_shadow")
 
-            T_ref = 0.01 + (coords['x'] + coords['y'] + coords['z'])*time
+            T_ref = 0.01 + (coords['x']*coords['y'] + coords['z']*coords['y'] + coords['z']*coords['x'])*time
             T_diff_max = np.argmax(abs(T-T_ref))
             assert np.allclose(T_ref, T, rtol=1e-7, atol=1e-12),\
                         f"Max diff ({time}): {abs(T-T_ref).max()} {T[T_diff_max]} {T_ref[T_diff_max]}"
@@ -26,13 +26,17 @@ class TestFoamVariableTransfer(unittest.TestCase):
                                                  time,
                                                  variable="whf_shadow")
 
-            eq = np.logical_or(np.logical_or(coords['x'] < 1e-6, coords['y'] < 1e-6),
-                               coords['z'] < 1e-6)
-
+            sum_ = 0
             whf_ref = np.zeros_like(whf)
-            whf_ref[eq] = - time
-            whf_ref[~eq] = time
+            for comp in ('x', 'y', 'z'):
+                low = coords[comp]<coords[comp].min()+1e-6
+                high = coords[comp]>(coords[comp].max()-1e-6)
+                sum_ += np.sum(low) + np.sum(high)
+                c = ('y', 'z') if comp == 'x' else ('z', 'x') if comp == 'y' else ('x', 'y')
+                whf_ref[low] = -time*(coords[c[0]][low] + coords[c[1]][low])
+                whf_ref[high] = time*(coords[c[0]][high] + coords[c[1]][high])
 
+            assert sum_ == whf.size, f"{sum_} {whf.size}"
 
             T_diff_max = np.argmax(abs(whf-whf_ref))
             assert np.allclose(whf_ref, whf, rtol=1e-7, atol=1e-12),\
