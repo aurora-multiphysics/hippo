6f683e23e00997bea873b4b683d0b7575af8fe4d
Matthew Falcone <mapfalcone@gmail.com>
2025-09-17 13:35:59 +0100

    Implement initial MWE FoamBCBase class

--- include/bcs/FoamBCBase.h
+++ include/bcs/FoamBCBase.h
@@ -1,10 +1,31 @@
+#pragma once
+
+#include "Coupleable.h"
 #include "InputParameters.h"
 #include "MooseObject.h"
+#include "MooseTypes.h"
+#include "MooseVariableConstMonomial.h"
+#include "MooseVariableFE.h"
+#include "MooseVariableFieldBase.h"
+#include "volFieldsFwd.H"
+#include "FoamMesh.h"
 
-class FoamBCBase : public MooseObject
+class FoamBCBase : public MooseObject, public Coupleable
 {
 public:
   static InputParameters validParams();
 
   explicit FoamBCBase(const InputParameters & params);
+
+  virtual void imposeBoundaryCondition() = 0;
+
+protected:
+  const MooseVariableFieldBase & getVariable();
+
+  Real variableValueAtElement(const libMesh::Elem * elem);
+
+  std::vector<Real> getVariableArray(int subdomainId);
+
+  const MooseVariableFieldBase & _v;
+  FoamMesh * _mesh;
 };
--- src/bcs/FoamBCBase.C
+++ src/bcs/FoamBCBase.C
@@ -1,17 +1,87 @@
+#include "Coupleable.h"
 #include "FoamBCBase.h"
+#include "FoamProblem.h"
 #include "MooseObject.h"
+#include "MooseVariableFieldBase.h"
 #include "Registry.h"
+#include <vector>
 
-registerMooseObject("hippoApp", FoamBCBase);
+// remove after improved tests
+// registerMooseObject("hippoApp", FoamBCBase);
+
+namespace
+{
+inline bool
+is_constant_monomial(const MooseVariableFieldBase & var)
+{
+  return var.order() == libMesh::Order::CONSTANT && var.feType().family == FEFamily::MONOMIAL;
+}
+}
 
 InputParameters
 FoamBCBase::validParams()
 {
   InputParameters params = MooseObject::validParams();
+  params.addRequiredParam<std::string>("foam_field",
+                                       "Name of a Foam field. e.g. T (temperature) U (velocity)");
+  params.addRequiredCoupledVar("v", "MOOSE variable to impose as the boundary condition");
+
   params.registerSystemAttributeName("FoamBC");
   params.registerBase("FoamBC");
 
   return params;
 }
 
-FoamBCBase::FoamBCBase(const InputParameters & params) : MooseObject(params) {}
+FoamBCBase::FoamBCBase(const InputParameters & params)
+  : MooseObject(params), Coupleable(this, false), _v(getVariable())
+{
+  auto * problem = dynamic_cast<FoamProblem *>(&_c_fe_problem);
+  if (!problem)
+    mooseError("FoamBC system can only be used with FoamProblem");
+  _mesh = &problem->mesh();
+}
+
+const MooseVariableFieldBase &
+FoamBCBase::getVariable()
+{
+
+  auto variable_name = *parameters().getCoupledVariableParamNames().begin();
+  auto * var = getFieldVar(variable_name, 0);
+  if (!is_constant_monomial(*var))
+  {
+    mooseError("variable '",
+               variable_name,
+               "' (",
+               var->name(),
+               ") must have:\n"
+               "  family = MONOMIAL\n"
+               "  order = CONSTANT\n");
+  }
+  return *var;
+}
+
+Real
+FoamBCBase::variableValueAtElement(const libMesh::Elem * elem)
+{
+  auto & sys = _v.sys();
+  auto dof = elem->dof_number(sys.number(), _v.number(), 0);
+  return sys.solution()(dof);
+}
+
+std::vector<Real>
+FoamBCBase::getVariableArray(int subdomainId)
+{
+  size_t patch_count = _mesh->getPatchCount(subdomainId);
+  size_t patch_offset = _mesh->getPatchOffset(subdomainId);
+
+  std::vector<Real> var_array(patch_count);
+  for (size_t j = 0; j < patch_count; ++j)
+  {
+    auto elem = patch_offset + j;
+    auto elem_ptr = _mesh->getElemPtr(elem + _mesh->rank_element_offset);
+    assert(elem_ptr);
+    var_array[j] = variableValueAtElement(elem_ptr);
+  }
+
+  return var_array;
+}
