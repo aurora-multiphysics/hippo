62b6e51aa6f3feaca87220bb4deb8891d7643aac
Matthew Falcone <mapfalcone@gmail.com>
2025-09-19 17:18:14 +0100

    Add table output for OpenFOAM variables

--- include/problems/FoamProblem.h
+++ include/problems/FoamProblem.h
@@ -19,7 +19,7 @@ public:
   virtual void syncSolutions(Direction /* dir */) override;
   virtual bool converged(const unsigned int /* nl_sys_num */) override { return true; }
   virtual void addExternalVariables() override {};
-
+  virtual void initialSetup() override;
   using ExternalProblem::mesh;
   virtual FoamMesh const & mesh() const override { return *_foam_mesh; }
   virtual FoamMesh & mesh() override { return *_foam_mesh; }
@@ -42,9 +42,13 @@ public:
 
   Hippo::FoamSolver & solver() { return _solver; }
 
-  void addShadowVariable(FoamVariableField * var);
+  // Add MOOSE variable that shadows a Foam counterpart
+  void addFoamVariable(FoamVariableField * var);
 
 protected:
+  // check FoamVariables and print summarising table
+  void verifyFoamVariables();
+
   FoamMesh * _foam_mesh = nullptr;
   Hippo::FoamSolver _solver;
   std::vector<FoamVariableField *> _shadow_variables;
