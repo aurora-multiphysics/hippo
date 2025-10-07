3c30f86d0d82cf2a5dd3a6a3d8762d0e251f3a19
Matthew Falcone <mapfalcone@gmail.com>
2025-10-07 16:51:35 +0100

    Add test FoamPostprocessor for input syntax

---
+++ test/src/postprocessors/FoamTestPostprocessor.C
@@ -0,0 +1,45 @@
+#include "FoamPostprocessorBase.h"
+#include "FoamTestPostprocessor.h"
+#include "Registry.h"
+
+registerMooseObject("hippoApp", FoamTestPostprocessor);
+
+InputParameters
+FoamTestPostprocessor::validParams()
+{
+  return FoamPostprocessorBase::validParams();
+}
+
+FoamTestPostprocessor::FoamTestPostprocessor(const InputParameters & params)
+  : FoamPostprocessorBase(params), _value(0.)
+{
+}
+
+void
+FoamTestPostprocessor::execute()
+{
+  _value = 1.;
+}
+
+void
+FoamTestPostprocessor::initialize()
+{
+  _value = 0.;
+}
+
+void
+FoamTestPostprocessor::threadJoin(const UserObject & uo)
+{
+}
+
+void
+FoamTestPostprocessor::finalize()
+{
+  gatherSum(_value);
+}
+
+PostprocessorValue
+FoamTestPostprocessor::getValue() const
+{
+  return _value;
+}
