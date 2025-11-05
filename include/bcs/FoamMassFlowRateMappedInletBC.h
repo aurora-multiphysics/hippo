e2366dc7210dc230f77d3ec521d958cc630570a9
Matthew Falcone <mapfalcone@gmail.com>
2025-11-05 15:18:00 +0000

    Add mapped inlet base class

--- include/bcs/FoamMassFlowRateMappedInletBC.h
+++ include/bcs/FoamMassFlowRateMappedInletBC.h
@@ -1,7 +1,6 @@
-#include "FoamBCBase.h"
-#include "UPstream.H"
+#include "FoamMappedInletBCBase.h"
 
-class FoamMassFlowRateMappedInletBC : public FoamBCBase, public PostprocessorInterface
+class FoamMassFlowRateMappedInletBC : public FoamMappedInletBCBase
 {
 public:
   static InputParameters validParams();
@@ -9,37 +8,4 @@ public:
   FoamMassFlowRateMappedInletBC(const InputParameters & params);
 
   virtual void imposeBoundaryCondition() override;
-
-  virtual void initialSetup() override;
-
-  virtual ~FoamMassFlowRateMappedInletBC()
-  {
-    if (Foam::UPstream::parRun())
-      Foam::UPstream::freeCommunicator(_foam_comm);
-  }
-
-protected:
-  PostprocessorName _pp_name;
-
-  Foam::vector _offset;
-
-  std::map<int, std::vector<int>> _send_map;
-
-  std::map<int, std::vector<int>> _recv_map;
-
-  Foam::label _foam_comm;
-
-  MPI_Comm _mpi_comm;
-
-  void createPatchProcMap();
-
-  template <typename T>
-  Foam::Field<T> getMappedArray(const Foam::word & name);
-
-  bool intersectMapPlane(const Foam::fvMesh & mesh, Real cart_bbox[6]);
-
-  void createMapComm(const Foam::fvMesh & mesh,
-                     Foam::vectorField face_centres,
-                     std::vector<int> & send_process,
-                     std::vector<int> & recv_process);
 };
