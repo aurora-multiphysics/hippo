691b27011c83bf6ee1847d84cb8db160e3ead4cf
Matthew Falcone <mapfalcone@gmail.com>
2025-11-06 17:25:48 +0000

    Implement different scaling approaches for the scalar bulk mapped inlet

--- include/bcs/FoamScalarBulkMappedInletBC.h
+++ include/bcs/FoamScalarBulkMappedInletBC.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "FoamMappedInletBCBase.h"
+#include "MooseEnum.h"
 
 class FoamScalarBulkMappedInletBC : public FoamMappedInletBCBase
 {
@@ -9,4 +10,10 @@ public:
   FoamScalarBulkMappedInletBC(const InputParameters & params);
 
   virtual void imposeBoundaryCondition() override;
+
+protected:
+  MooseEnum _scale_method;
+
+  template <typename T>
+  T applyScaleMethod(T & var, const Real bulk_ref, const Real bulk);
 };
