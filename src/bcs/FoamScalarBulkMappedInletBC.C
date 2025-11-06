6bee777d9532970b16cf071c7c6bd01bf280420f
Matthew Falcone <mapfalcone@gmail.com>
2025-11-06 17:25:48 +0000

    Implement different scaling approaches for the scalar bulk mapped inlet

--- src/bcs/FoamScalarBulkMappedInletBC.C
+++ src/bcs/FoamScalarBulkMappedInletBC.C
@@ -1,5 +1,6 @@
 #include "FoamScalarBulkMappedInletBC.h"
 #include "InputParameters.h"
+#include "MooseError.h"
 #include "MooseTypes.h"
 #include "PstreamReduceOps.H"
 #include "Registry.h"
@@ -13,12 +14,18 @@ InputParameters
 FoamScalarBulkMappedInletBC::validParams()
 {
   auto params = FoamMappedInletBCBase::validParams();
+  MooseEnum scaleEnum("SCALE SUBTRACT", "SCALE");
+  params.addParam<MooseEnum>("scale_method",
+                             scaleEnum,
+                             "Method used to maintain inlet bulk variable. "
+                             "SCALE means the variable is multiplied by a factor, "
+                             "SUBTRACT means the variable is reduced by constant.");
 
   return params;
 }
 
 FoamScalarBulkMappedInletBC::FoamScalarBulkMappedInletBC(const InputParameters & params)
-  : FoamMappedInletBCBase(params)
+  : FoamMappedInletBCBase(params), _scale_method(params.get<MooseEnum>("scale_method"))
 {
 }
 
@@ -41,5 +48,24 @@ FoamScalarBulkMappedInletBC::imposeBoundaryCondition()
   auto & var = const_cast<Foam::fvPatchField<Foam::scalar> &>(
       boundary_patch.lookupPatchField<Foam::volScalarField, double>("T"));
 
-  var == var_map * _pp_value / var_bulk;
+  var == applyScaleMethod(var_map, _pp_value, var_bulk);
+}
+
+template <typename T>
+T
+FoamScalarBulkMappedInletBC::applyScaleMethod(T & var, const Real bulk_ref, const Real bulk)
+{
+  if (_scale_method == "SCALE")
+  {
+    std::cout << bulk << " " << bulk_ref << std::endl;
+    return (var * bulk_ref / bulk)();
+  }
+  else if (_scale_method == "SUBTRACT")
+  {
+    return (var + bulk_ref - bulk)();
+  }
+  else
+  {
+    mooseError("Invalid scale method '", _scale_method, "'.");
+  }
 }
