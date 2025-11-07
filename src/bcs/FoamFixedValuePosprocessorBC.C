6ff3a0e0ee90004c718c2ee5460f5a3d8e172026
Matthew Falcone <mapfalcone@gmail.com>
2025-11-07 10:45:31 +0000

    Implement fixed value and fixed gradient postprocessor BCs

---
+++ src/bcs/FoamFixedValuePosprocessorBC.C
@@ -0,0 +1,33 @@
+#include "FoamFixedValuePostprocessorBC.h"
+
+InputParameters
+FoamFixedValuePostprocessorBC::validParams()
+{
+  return FoamPostprocessorBCBase::validParams();
+}
+
+FoamFixedValuePostprocessorBC::FoamFixedValuePostprocessorBC(const InputParameters & params)
+  : FoamPostprocessorBCBase(params)
+{
+}
+
+void
+FoamFixedValuePostprocessorBC::imposeBoundaryCondition()
+{
+  auto & foam_mesh = _mesh->fvMesh();
+
+  // Get subdomains this FoamBC acts on
+  // TODO: replace with BoundaryRestriction member functions once FoamMesh is updated
+  auto subdomains = _mesh->getSubdomainIDs(_boundary);
+  for (auto subdomain : subdomains)
+  {
+    // Get underlying field from OpenFOAM boundary patch
+    auto & foam_var = const_cast<Foam::fvPatchField<double> &>(
+        foam_mesh.boundary()[subdomain].lookupPatchField<Foam::volScalarField, double>(
+            _foam_variable));
+
+    assert(var_array.size() == static_cast<std::size_t>(foam_var.size()));
+
+    std::fill(foam_var.begin(), foam_var.end(), _pp_value);
+  }
+}
