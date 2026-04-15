1b0e2d0679567d007aebc96edfa5effe2e646646
Matthew Falcone <mapfalcone@gmail.com>
2026-04-15 15:26:58 +0100

    Remove gold exodus files from simple diffusion test

---
+++ test/tests/kernels/simple_diffusion/test.py
@@ -0,0 +1,20 @@
+"""Test module for basic MOOSE functionality within Hippo"""
+
+from pathlib import Path
+import unittest
+
+from read_hippo_data import read_moose_exodus_data
+
+RUN_DIR = Path(__file__).parent
+
+
+class TestDiffusionProblem(unittest.TestCase):
+    """Test that simple kernels work within Hippo"""
+
+    def test_analytical_solution(self):
+        """Test diffusion kernel against analytical solution"""
+        coords, u = read_moose_exodus_data(RUN_DIR / "simple_diffusion_out.e", 1.0, "u")
+
+        assert all(abs(coords["x"] - u) < 3e-8), (
+            f"Max diff: {abs(coords['x'] - u).max()}"
+        )
