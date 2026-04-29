81492e5780ede21a3ac7497af76901a053a0e09a
Matthew Falcone <mapfalcone@gmail.com>
2026-04-29 09:41:50 +0100

    Update based on code review

--- test/tests/kernels/simple_diffusion/test.py
+++ test/tests/kernels/simple_diffusion/test.py
@@ -8,6 +8,10 @@ from read_hippo_data import read_moose_exodus_data
 RUN_DIR = Path(__file__).parent
 
 
+def u_analytic(x):
+    return x
+
+
 class TestDiffusionProblem(unittest.TestCase):
     """Test that simple kernels work within Hippo"""
 
@@ -15,6 +19,6 @@ class TestDiffusionProblem(unittest.TestCase):
         """Test diffusion kernel against analytical solution"""
         coords, u = read_moose_exodus_data(RUN_DIR / "simple_diffusion_out.e", 1.0, "u")
 
-        assert all(abs(coords["x"] - u) < 3e-8), (
+        assert all(abs(u_analytic(coords["x"]) - u) < 3e-8), (
             f"Max diff: {abs(coords['x'] - u).max()}"
         )
