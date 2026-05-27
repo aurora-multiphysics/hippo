3f814e1fc6b0fb266b38284e03bb17e40a312ee4
Matthew Falcone <mapfalcone@gmail.com>
2026-05-27 11:30:20 +0100

    Add tests for FoamHeatTransferCoeff

--- src/variables/FoamVariableField.C
+++ src/variables/FoamVariableField.C
@@ -4,6 +4,7 @@
 #include "InputParameters.h"
 #include "MooseObject.h"
 #include "MooseTypes.h"
+#include "hippoUtils.h"
 
 registerMooseObject("hippoApp", FoamVariableField);
 
@@ -41,19 +42,9 @@ FoamVariableField::transferVariable()
   auto & foam_mesh = _mesh->fvMesh();
   for (auto subdomain : _mesh->getSubdomainList())
   {
-    size_t patch_count = _mesh->getPatchCount(subdomain);
-    size_t patch_offset = _mesh->getPatchOffset(subdomain);
-
     auto & var = foam_mesh.boundary()[subdomain].lookupPatchField<Foam::volScalarField, double>(
         _foam_variable);
-    for (size_t j = 0; j < patch_count; ++j)
-    {
-      auto elem = patch_offset + j;
-      auto elem_ptr = _mesh->getElemPtr(elem + _mesh->rank_element_offset);
-      assert(elem_ptr);
-      auto dof_t = elem_ptr->dof_number(moose_var.sys().number(), moose_var.number(), 0);
-      moose_var.sys().solution().set(dof_t, var[j]);
-    }
+    Hippo::internal::copyFieldFoamToMoose(*_mesh, var, moose_var, subdomain);
   }
 
   moose_var.sys().solution().close();
