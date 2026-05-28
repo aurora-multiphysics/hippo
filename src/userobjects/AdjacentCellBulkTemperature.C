1e12d2d47b2f1c3468b5d20ca43d3eef0d0dcc6f
Matthew Falcone <mapfalcone@gmail.com>
2026-05-28 14:18:25 +0100

    Get MultiAppGeneralFieldUserObjectTransfer to work with adjacent bulk uo

--- src/userobjects/AdjacentCellBulkTemperature.C
+++ src/userobjects/AdjacentCellBulkTemperature.C
@@ -2,15 +2,22 @@
 #include "HippoBase.h"
 #include "FoamProblem.h"
 #include "MooseTypes.h"
+#include "libmesh/libmesh_common.h"
+#include "petsclog.h"
+#include <Field.H>
 #include <ListOps.H>
 #include <Pstream/mpi/PstreamGlobals.H>
 #include <PstreamReduceOps.H>
 #include <UList.H>
+#include <UPstream.H>
 #include <VectorSpace.H>
+#include <face.H>
 #include <fvMesh.H>
 #include <ops.H>
 #include <scalar.H>
 #include <scalarField.H>
+#include <vector>
+#include <vectorField.H>
 
 registerMooseObject("hippoApp", AdjacentCellBulkTemperature);
 
@@ -26,8 +33,80 @@ AdjacentCellBulkTemperature::validParams()
 AdjacentCellBulkTemperature::AdjacentCellBulkTemperature(const InputParameters & params)
   : GeneralUserObject(params),
     HippoBase(this),
-    _foam_patch{getFoamPatch(getParam<SubdomainName>("boundary"))}
+    _foam_patch{getFoamPatch(getParam<SubdomainName>("boundary"))},
+    _gl_face_centres{},
+    _gl_t_adjacent{}
+
 {
+}
+
+void
+AdjacentCellBulkTemperature::initialize()
+{
+  int size = _foam_patch.size();
+  int gl_size;
+  int nProcs{Foam::UPstream::nProcs()};
+  MPI_Comm comm{(nProcs == 1) ? MPI_COMM_WORLD : Foam::PstreamGlobals::MPI_COMM_FOAM};
+
+  MPI_Allreduce(&size, &gl_size, 1, MPI_INT, MPI_SUM, comm);
+  _gl_face_centres.resize(gl_size, {-1, -1, -1});
+  _gl_t_adjacent.resize(gl_size, -1);
+}
+
+void
+AdjacentCellBulkTemperature::execute()
+{
+  Foam::vectorField face_centres = _foam_patch.Cf();
+  int nProcs{Foam::UPstream::nProcs()};
+
+  std::vector<int> sizes{nProcs};
+  std::vector<int> displs{nProcs};
+  std::vector<int> face_sizes{nProcs};
+  std::vector<int> face_displs{nProcs};
+
+  MPI_Comm comm{(nProcs == 1) ? MPI_COMM_WORLD : Foam::PstreamGlobals::MPI_COMM_FOAM};
+
+  const int l_num_faces = face_centres.size();
+  MPI_Allgather(&l_num_faces, 1, MPI_INT, sizes.data(), 1, MPI_INT, comm);
+  displs[0] = 0;
+  for (int i = 0; i < nProcs - 1; ++i)
+  {
+    displs[i + 1] = displs[i] + sizes[i];
+  }
+
+  for (int i = 0; i < nProcs; ++i)
+  {
+    face_sizes[i] = sizes[i] * 3;
+    face_displs[i] = displs[i] * 3;
+  }
+
+  MPI_Allgatherv(face_centres.data(),
+                 face_centres.size() * 3,
+                 MPI_DOUBLE,
+                 _gl_face_centres.data(),
+                 face_sizes.data(),
+                 face_displs.data(),
+                 MPI_DOUBLE,
+                 comm);
+
+  Foam::scalarField l_t_adjacent{face_centres.size()};
+
+  const Foam::fvMesh & foam_mesh{getFoamProblem().mesh().fvMesh()};
+  const auto & T = foam_mesh.lookupObject<Foam::volScalarField>(getParam<std::string>("T_name"));
+  for (int i = 0; i < l_t_adjacent.size(); ++i)
+  {
+    int idx = _foam_patch.faceCells()[i];
+    l_t_adjacent[i] = T[idx];
+  }
+
+  MPI_Allgatherv(l_t_adjacent.data(),
+                 l_t_adjacent.size(),
+                 MPI_DOUBLE,
+                 _gl_t_adjacent.data(),
+                 sizes.data(),
+                 displs.data(),
+                 MPI_DOUBLE,
+                 comm);
 }
 
 const Foam::fvPatch &
@@ -41,24 +120,18 @@ AdjacentCellBulkTemperature::getFoamPatch(const std::string & boundary)
 Real
 AdjacentCellBulkTemperature::spatialValue(const Point & point) const
 {
-  const Foam::fvMesh & foam_mesh{getFoamProblem().mesh().fvMesh()};
-
-  // Boundary face centres
-  const Foam::vectorField & face_centres = _foam_patch.Cf();
   const Foam::point p_moose{point(0), point(1), point(2)};
-  Foam::scalar minDist = Foam::rootVGreat;
   Foam::label idx;
-
-  for (int i = 0; i < face_centres.size(); ++i)
+  Foam::scalar minDist = Foam::VGREAT;
+  for (int i = 0; i < _gl_face_centres.size(); ++i)
   {
-    const Foam::point & p = face_centres[i];
+    const Foam::point & p = _gl_face_centres[i];
     const Foam::scalar dist{Foam::mag(p_moose - p)};
     if (dist < minDist)
     {
-      idx = _foam_patch.faceCells()[i];
+      idx = i;
       minDist = dist;
     }
   }
-
-  return foam_mesh.lookupObject<Foam::volScalarField>(getParam<std::string>("T_name"))[idx];
+  return _gl_t_adjacent[idx];
 }
