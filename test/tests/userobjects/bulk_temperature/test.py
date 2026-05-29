5eaf7462d09266e2082aae51ece7f43bea200f84
Matthew Falcone <mapfalcone@gmail.com>
2026-05-29 10:22:35 +0100

    Add test for MultiAppGeneralFieldUserObjectTransfers in parallel

--- test/tests/userobjects/bulk_temperature/test.py
+++ test/tests/userobjects/bulk_temperature/test.py
@@ -10,16 +10,22 @@ class TestAdjacentCellBulkTemperature(unittest.TestCase):
 
     def test_adjacent_cell(self):
         """Compares output against expected solution"""
-        times, temp = np.loadtxt(
-            "adjacent_bulk_temperature_out.csv", unpack=True, skiprows=1, delimiter=","
+        times, temp1 = np.loadtxt(
+            "main_out.csv", unpack=True, skiprows=1, delimiter=","
+        )
+        times, temp2 = np.loadtxt(
+            "main_out_hippo0.csv", unpack=True, skiprows=1, delimiter=","
         )
 
-        for t, T in zip(times, temp):
+        for t, T1, T2 in zip(times, temp1, temp2):
             x0 = 0.5 * 10.0 / 50
             y0 = 0.5
             z0 = 0.5
 
             T_ref = 0.01 + (x0 * y0 + y0 * z0 + x0 * z0) * t
-            assert abs(T_ref - T) < 1e-12, (
-                f"Check temperatures match expected values {T_ref} vs {T}"
+            assert abs(T_ref - T1) < 1e-12, (
+                f"Check temperatures match expected values {T_ref} vs {T1}"
+            )
+            assert abs(T_ref - T2) < 1e-12, (
+                f"Check temperatures match expected values {T_ref} vs {T2}"
             )
