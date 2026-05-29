a90c6db8cc4b229dc3b8de47a72578aad0b7eb78
Matthew Falcone <mapfalcone@gmail.com>
2026-05-29 09:41:10 +0100

    Use KDTree for more efficient location searching

--- src/userobjects/AdjacentCellBulkTemperature.C
+++ src/userobjects/AdjacentCellBulkTemperature.C
@@ -2,7 +2,6 @@
 #include "HippoBase.h"
 #include "FoamProblem.h"
 #include "MooseTypes.h"
-#include "libmesh/libmesh_common.h"
 #include "petsclog.h"
 #include <Field.H>
 #include <ListOps.H>
@@ -13,11 +12,13 @@
 #include <VectorSpace.H>
 #include <face.H>
 #include <fvMesh.H>
+#include <memory>
 #include <ops.H>
 #include <scalar.H>
 #include <scalarField.H>
 #include <vector>
 #include <vectorField.H>
+#include <KDTree.h>
 
 registerMooseObject("hippoApp", AdjacentCellBulkTemperature);
 
@@ -27,6 +28,9 @@ AdjacentCellBulkTemperature::validParams()
   InputParameters params = GeneralUserObject::validParams();
   params.addRequiredParam<SubdomainName>("boundary", "Boundary this object applies to.");
   params.addParam<std::string>("T_name", "T", "Temperature name in the OpenFOAM simulation.");
+  params.addParam<int>("max_leaf_size", 10, "Maximum leaf size to use for the KD tree search.");
+  params.addParam<bool>(
+      "reconstruct_tree", false, "Whether to rebuild the KD tree on each execution.");
   return params;
 }
 
@@ -34,8 +38,9 @@ AdjacentCellBulkTemperature::AdjacentCellBulkTemperature(const InputParameters &
   : GeneralUserObject(params),
     HippoBase(this),
     _foam_patch{getFoamPatch(getParam<SubdomainName>("boundary"))},
-    _gl_face_centres{},
-    _gl_t_adjacent{}
+    _kd_centres{},
+    _gl_t_adjacent{},
+    _kd_tree{}
 
 {
 }
@@ -49,47 +54,65 @@ AdjacentCellBulkTemperature::initialize()
   MPI_Comm comm{(nProcs == 1) ? MPI_COMM_WORLD : Foam::PstreamGlobals::MPI_COMM_FOAM};
 
   MPI_Allreduce(&size, &gl_size, 1, MPI_INT, MPI_SUM, comm);
-  _gl_face_centres.resize(gl_size, {-1, -1, -1});
-  _gl_t_adjacent.resize(gl_size, -1);
+  _gl_t_adjacent.resize(gl_size);
+  _kd_centres.resize(gl_size);
+
+  buildKDTree();
 }
 
 void
-AdjacentCellBulkTemperature::execute()
+AdjacentCellBulkTemperature::buildKDTree()
 {
   Foam::vectorField face_centres = _foam_patch.Cf();
+
   int nProcs{Foam::UPstream::nProcs()};
 
-  std::vector<int> sizes{nProcs};
-  std::vector<int> displs{nProcs};
   std::vector<int> face_sizes{nProcs};
   std::vector<int> face_displs{nProcs};
+  _mpi_sizes.resize(nProcs);
+  _mpi_displs.resize(nProcs);
 
   MPI_Comm comm{(nProcs == 1) ? MPI_COMM_WORLD : Foam::PstreamGlobals::MPI_COMM_FOAM};
 
   const int l_num_faces = face_centres.size();
-  MPI_Allgather(&l_num_faces, 1, MPI_INT, sizes.data(), 1, MPI_INT, comm);
-  displs[0] = 0;
+  MPI_Allgather(&l_num_faces, 1, MPI_INT, _mpi_sizes.data(), 1, MPI_INT, comm);
+  _mpi_displs[0] = 0;
   for (int i = 0; i < nProcs - 1; ++i)
   {
-    displs[i + 1] = displs[i] + sizes[i];
+    _mpi_displs[i + 1] = _mpi_displs[i] + _mpi_sizes[i];
   }
 
   for (int i = 0; i < nProcs; ++i)
   {
-    face_sizes[i] = sizes[i] * 3;
-    face_displs[i] = displs[i] * 3;
+    face_sizes[i] = _mpi_sizes[i] * 3;
+    face_displs[i] = _mpi_displs[i] * 3;
   }
 
+  Foam::vectorField gl_face_centres(_gl_t_adjacent.size());
   MPI_Allgatherv(face_centres.data(),
                  face_centres.size() * 3,
                  MPI_DOUBLE,
-                 _gl_face_centres.data(),
+                 gl_face_centres.data(),
                  face_sizes.data(),
                  face_displs.data(),
                  MPI_DOUBLE,
                  comm);
 
-  Foam::scalarField l_t_adjacent{face_centres.size()};
+  for (int i = 0; i < gl_face_centres.size(); ++i)
+  {
+    const Foam::point & p = gl_face_centres[i];
+    _kd_centres[i] = Point(p.x(), p.y(), p.z());
+  }
+  _kd_tree = std::make_unique<KDTree>(_kd_centres, getParam<int>("max_leaf_size"));
+}
+
+void
+AdjacentCellBulkTemperature::execute()
+{
+  if (getParam<bool>("reconstruct_tree"))
+    buildKDTree();
+
+  Foam::scalarField l_t_adjacent{_foam_patch.Cf().size()};
 
   const Foam::fvMesh & foam_mesh{getFoamProblem().mesh().fvMesh()};
   const auto & T = foam_mesh.lookupObject<Foam::volScalarField>(getParam<std::string>("T_name"));
@@ -99,12 +122,14 @@ AdjacentCellBulkTemperature::execute()
     l_t_adjacent[i] = T[idx];
   }
 
+  int nProcs{Foam::UPstream::nProcs()};
+  MPI_Comm comm{(nProcs == 1) ? MPI_COMM_WORLD : Foam::PstreamGlobals::MPI_COMM_FOAM};
   MPI_Allgatherv(l_t_adjacent.data(),
                  l_t_adjacent.size(),
                  MPI_DOUBLE,
                  _gl_t_adjacent.data(),
-                 sizes.data(),
-                 displs.data(),
+                 _mpi_sizes.data(),
+                 _mpi_displs.data(),
                  MPI_DOUBLE,
                  comm);
 }
@@ -121,17 +146,7 @@ Real
 AdjacentCellBulkTemperature::spatialValue(const Point & point) const
 {
   const Foam::point p_moose{point(0), point(1), point(2)};
-  Foam::label idx;
-  Foam::scalar minDist = Foam::VGREAT;
-  for (int i = 0; i < _gl_face_centres.size(); ++i)
-  {
-    const Foam::point & p = _gl_face_centres[i];
-    const Foam::scalar dist{Foam::mag(p_moose - p)};
-    if (dist < minDist)
-    {
-      idx = i;
-      minDist = dist;
-    }
-  }
-  return _gl_t_adjacent[idx];
+  std::vector<size_t> idx_vec;
+  _kd_tree->neighborSearch(point, 1, idx_vec);
+  return _gl_t_adjacent[idx_vec.at(0)];
 }
