7548fb801f59b8f47985936ebab0d193341077d0
Matthew Falcone <mapfalcone@gmail.com>
2026-06-05 14:34:28 +0100

    Improve comments and docstrings for tests

--- test/tests/userobjects/bulk_temperature/test.py
+++ test/tests/userobjects/bulk_temperature/test.py
@@ -18,10 +18,12 @@ class TestAdjacentCellBulkTemperature(unittest.TestCase):
         )
 
         for t, T1, T2 in zip(times, temp1, temp2):
+            # Expected adjacent cell expected at this location
             x0 = 0.5 * 10.0 / 50
             y0 = 0.5
             z0 = 0.5
 
+            # Calculate T used in solver
             T_ref = 0.01 + (x0 * y0 + y0 * z0 + x0 * z0) * t
             assert abs(T_ref - T1) < 1e-12, (
                 f"Check temperatures match expected values {T_ref} vs {T1}"
