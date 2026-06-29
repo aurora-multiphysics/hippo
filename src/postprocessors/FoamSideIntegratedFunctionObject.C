7e31c62c7116c9f865965ef23216b5946770dcc0
Matthew Falcone <mapfalcone@gmail.com>
2026-06-29 12:34:28 +0100

    Update variables with HippoInterface

--- src/postprocessors/FoamSideIntegratedFunctionObject.C
+++ src/postprocessors/FoamSideIntegratedFunctionObject.C
@@ -26,7 +26,7 @@ FoamSideIntegratedFunctionObject::FoamSideIntegratedFunctionObject(const InputPa
 std::unique_ptr<Foam::functionObject>
 FoamSideIntegratedFunctionObject::createFunctionObject(const std::string & fo_name)
 {
-  auto fo_dict = _fv_mesh.time().controlDict().lookupOrDefault(fo_name, Foam::dictionary());
+  auto fo_dict = _foam_time.controlDict().lookupOrDefault(fo_name, Foam::dictionary());
 
   Foam::wordList patch_names(_boundary.begin(), _boundary.end());
 
@@ -36,12 +36,12 @@ FoamSideIntegratedFunctionObject::createFunctionObject(const std::string & fo_na
   if (fo_name == "wallHeatFlux")
   {
     return std::make_unique<Foam::functionObjects::wallHeatFlux>(
-        "wallHeatFlux", _fv_mesh.time(), fo_dict);
+        "wallHeatFlux", _foam_time, fo_dict);
   }
   else // wallShearStress
   {
     return std::make_unique<Foam::functionObjects::wallShearStress>(
-        "wallShearStress", _fv_mesh.time(), fo_dict);
+        "wallShearStress", _foam_time, fo_dict);
   }
 }
 
