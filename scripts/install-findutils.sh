bd9e046150151460c1e939d1f40165ad39511838
Matthew Falcone <mapfalcone@gmail.com>
2026-08-11 11:27:39 +0100

    install up-to-date version of findutils in dev container

---
+++ scripts/install-findutils.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+version="4.10.0"
+prefix="/opt/findutils-${version}"
+build_dir="$(mktemp -d /tmp/findutils-build.XXXXXX)"
+
+cleanup()
+{
+    rm -rf "${build_dir}"
+}
+trap cleanup EXIT
+
+cd "${build_dir}"
+wget --quiet "https://ftp.gnu.org/gnu/findutils/findutils-${version}.tar.xz"
+tar -xf "findutils-${version}.tar.xz"
+cd "findutils-${version}"
+
+./configure \
+    --prefix="${prefix}" \
+    --disable-nls
+
+make -j"$(nproc)"
+make install
+
+mkdir -p /usr/local/bin
+ln -sfn "${prefix}/bin/find" /usr/local/bin/find
+
+# Verify the comma-separated type syntax used by OpenFOAM's CleanFunctions.
+/usr/local/bin/find --version
+/usr/local/bin/find /tmp -maxdepth 0 -type f,l -print
